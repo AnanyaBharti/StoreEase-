642d02c497e131a78e27168a5f940576e9ecc85a
Ananya Bharti <ananyabharti@Ananyas-MacBook-Air.local>
2024-10-05 09:09:31 +0530

    Created Crud Api

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+// const express = require('express');    //old syntax to import expresss
+
+import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //allows us to accept JSON data in the req.body ........It is a middleware which is just a function used to send request to the client..
+
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log("Server started at http://localhost:" + PORT);
+});
+
+
